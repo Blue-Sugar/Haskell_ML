41c2241b7d6e0cacd9b69408b18fa23353ce86de
Blue_S <aoicchi0620@gmail.com>
2023-05-09 07:33:49 +0900

    Ch3 finish(具体的なニューラルネットワークは構築していない)

--- Ch3.hs
+++ Ch3.hs
@@ -1,6 +1,7 @@
 module Ch3 where
 import MatrixAndVector
 
+
 stepFunc :: Vector -> Vector
 stepFunc  = map (\x -> if x > 0 then 1 else 0) 
 
@@ -9,3 +10,8 @@ sigmoidFunc = map (\x -> 1.0 / (1.0 + exp (-1 * x)))
 
 reluFunc :: Vector -> Vector
 reluFunc = map (`max` 0)
+
+softmaxFunc :: Vector -> Vector
+softmaxFunc v = map (\x -> exp (x-max_of_v)/sum_of_exp) v
+    where sum_of_exp = sum  (map (\x -> exp (x - max_of_v)) v) 
+          max_of_v = maximum v
--- Ch3Test.hs
+++ Ch3Test.hs
@@ -9,15 +9,3 @@ funcTest f = map (\x -> f (fst x) == snd x)
 stepFuncTest :: [(Vector, Vector)]
 stepFuncTest = [([1.0, 0.5, 0.0, -0.5], [1.0, 1.0, 0.0, 0.0])]
 
-w1 = [[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]
-w2 = [[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]
-w3 = [[0.1, 0.3], [0.2, 0.4]]
-b1 = [0.1, 0.3, 0.5]
-b2 = [0.1, 0.2]
-b3 = [0.1, 0.2]
-
-x = [1.0, 0.5]
-
-z1 = sigmoidFunc (forward x w1 b1)
-z2 = sigmoidFunc (forward z1 w2 b2)
-z3 = forward z2 w3 b3
